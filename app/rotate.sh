3226f5b8f6c3a4e82bef629bb9bdf04c54456e18
eisaku <e_watanabe@dreamarts.co.jp>
2023-08-18 13:47:14 +0900

    mysqlのスロークエリログを有効化

--- app/rotate.sh
+++ app/rotate.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 mv ./logs/nginx/access.log ./logs/nginx/access.log.`date +%Y%m%d-%H%M%S`
+mv ./logs/mysql/mysql-slow.log ./logs/mysql/mysql-slow.log.`date +%Y%m%d-%H%M%S`
+
+bash ./restart_container.sh
